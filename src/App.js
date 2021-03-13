af282079eb92845c313520d5247540326014f35d
malopes21 <malopes21@gmail.com>
2021-03-13 16:10:24 -0300

    fix it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Table from './Table'
 
 class App extends React.Component {
     render() {
+
+        const characters = [
+            {
+                name: 'Marcos Lopes Silva',
+                job: 'Professor TI'
+            },
+            {
+                name: 'Fulano de Tal',
+                job: 'Trabalhador de Encanamento'
+            },
+            {
+                name: 'Beltrano o Tal',
+                job: 'Atendimento a Cliente'
+            }
+        ]
+
         return (
-            <div className="App">
-                <h1>opassssss!</h1>
+            <div className="container">
+                <Table characterData ={characters}/>
             </div>
         )
     }
---
+++ src/Table.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        )
+    })
+    return (
+        <tbody>{rows}</tbody>
+    )
+}
+
+class Table extends React.Component {
+    render() {
+        const {characterData} = this.props
+
+        return (
+            <table>
+               <TableHeader/>
+               <TableBody characterData={characterData} />
+            </table>
+        )
+    }
+}
+
+export default Table
